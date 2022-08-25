1b39255696fa729e4ebc0432d790f9bc888ddcfd
Alex <alexbrucely@gmail.com>
2022-08-25 16:15:45 -0700

    remove license bits

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,6 @@
 cluster_context="cluster1"
 # number of app waves in the environments directory
 environment_waves="3"
-LICENSE_KEY="${1:-$LICENSE_KEY}"
 
 # check to see if defined contexts exist
 if [[ $(kubectl config get-contexts | grep ${cluster_context}) == "" ]] ; then
@@ -17,40 +16,6 @@ if [[ $(kubectl config get-contexts | grep ${cluster_context}) == "" ]] ; then
   echo "Run 'kubectl config get-contexts' to see currently available contexts. If the clusters are available, please make sure that they are named correctly. Default is ${cluster_context}"
   exit 1;
 fi
-
-# check to see if license key variable was passed through, if not prompt for key
-if [[ ${LICENSE_KEY} == "" ]]
-  then
-    # provide license key
-    echo "Please provide your Gloo Mesh Enterprise License Key:"
-    read LICENSE_KEY
-fi
-
-# check OS type
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Linux
-        BASE64_LICENSE_KEY=$(echo -n "${LICENSE_KEY}" | base64 -w 0)
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        BASE64_LICENSE_KEY=$(echo -n "${LICENSE_KEY}" | base64)
-else
-        echo unknown OS type
-        exit 1
-fi
-
-# license stuff
-kubectl create ns gloo-mesh --context ${cluster_context}
-
-kubectl apply --context ${cluster_context} -f - <<EOF
-apiVersion: v1
-data:
-  gloo-mesh-license-key: ${BASE64_LICENSE_KEY}
-kind: Secret
-metadata:
-  name: gloo-mesh-license
-  namespace: gloo-mesh
-type: Opaque
-EOF
 
 # install argocd
 cd bootstrap-argocd
