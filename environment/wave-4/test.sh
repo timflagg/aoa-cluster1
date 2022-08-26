d7a78a74a3d5cbde6a879526cd12ce19af234479
Alex <alexbrucely@gmail.com>
2022-08-26 16:42:29 -0700

    update test

--- environment/wave-4/test.sh
+++ environment/wave-4/test.sh
@@ -2,5 +2,8 @@
 
 cluster_context="cluster1"
 
-./tools/wait-for-rollout.sh deployment ext-auth-server gloo-mesh-addons 10 ${cluster_context}
+
+./tools/wait-for-rollout.sh deployment ext-auth-service gloo-mesh-addons 10 ${cluster_context}
+./tools/wait-for-rollout.sh deployment rate-limiter gloo-mesh-addons 10 ${cluster_context}
+./tools/wait-for-rollout.sh deployment redis gloo-mesh-addons 10 ${cluster_context}
 
