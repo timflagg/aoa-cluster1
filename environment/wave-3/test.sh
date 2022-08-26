c3e7b1191ab48ff70fe41011de2c042c4a1d1ac4
Alex <alexbrucely@gmail.com>
2022-08-26 16:24:40 -0700

    Update test.sh

--- environment/wave-3/test.sh
+++ environment/wave-3/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cluster_context="cluster1"
 
-./tools/wait-for-rollout.sh deployment istiod istio-system 10 ${cluster_context}
 ./tools/wait-for-rollout.sh deployment istio-ingressgateway istio-gateways 10 ${cluster_context}
 ./tools/wait-for-rollout.sh deployment istio-eastwestgateway istio-gateways 10 ${cluster_context}
